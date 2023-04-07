c7578849389a761a386529ed4a5fc0905debd155
Michael Matus <michael@matus.at>
2023-04-07 22:42:59 +0200

    Add project files.

---
+++ IGpibHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Bev.IO.Gpib
+{
+    public interface IGpibHandler
+    {
+        void Remote(int address);
+
+        void Local(int address);
+
+        void Trigger(int address);
+
+        void Output(int address, string command);
+
+        string Enter(int address);
+    }
+}
